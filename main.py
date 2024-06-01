049a416e79c9eb6539e6a7593888be29c9797b60
Tushar Behalpade <tushar@192.168.1.4>
2024-06-01 16:57:37 +0530

    added credit card list for hdfc and axis

--- main.py
+++ main.py
@@ -14,13 +14,31 @@ if __name__ == "__main__":
         "ICICI Bank HPCL Super Saver Credit Card",
         "MakeMyTrip ICICI Bank Signature Credit Card"
       ]
+
+    credit_cards_hdfc = ["HDFC Bank Regalia Credit Card",
+                         "HDFC Bank Diners Club Credit Card",
+                         "HDFC Bank Millenia Credit Card",
+                         "HDFC Bank MoneyBack Credit Card",
+                         "HDFC Bank Platinum Plus Credit card"]
+
+    credit_cards_axis = ["Axis Bank Ace Credit card",
+                         "Axis Bank Flipkart Credit card",
+                         "Axis Bank Neo Credit card",
+                         "Axis Bank Vistara Credit card",
+                         "Axis Bank Magnus Credit card"]
+
     for bank in bank_name_list:
         b = Bank(bank)
         b.CreditCards = []
         if bank == "ICICI Bank":
             for cc in credit_cards_icici:
                 b.CreditCards.append(CreditCard(cc))
-
+        elif bank == "HDFC Bank":
+            for cc in credit_cards_hdfc:
+                b.CreditCards.append(CreditCard(cc))
+        elif bank == "Axis Bank":
+            for cc in credit_cards_axis:
+                b.CreditCards.append(CreditCard(cc))
         bank_list.append(b)
 
     for bank in bank_list:
